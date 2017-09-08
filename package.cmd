a55a21370110c9e6dca3c89afe9066ac2c36ec15
mazong1123 <mazong1123@gmail.com>
2017-09-08 12:56:57 +0800

    Added package.cmd.

--- package.cmd
+++ package.cmd
@@ -1,1 +1,4 @@
 REM Put your package scripts here.
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\heat.exe" dir ".\src" -cg appveyor-wix-sample -gg -sfrag -sreg -svb6 -template product -t wix.xslt -out appveyor-wix-sample.wxs
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe" appveyor-wix-sample.wxs
+"C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe" -b ".\src" .\appveyor-wix-sample.wixobj
